88e89a5294fe504b6b0c29e7af8560ef1fd32451
lukeska <luca.ubiali@gmail.com>
2021-03-28 17:39:27 +0100

    stats blocks and blog sections

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex h-full">
 		<div class="fixed lg:static bottom-0 w-full lg:w-24 lg:h-full z-10">
-			<div class="w-full lg:w-24 h-full bg-blue-800 relative">
+			<div class="w-full lg:fixed lg:w-24 h-full bg-blue-800 relative">
 				<a
 					href="#"
 					class="hidden absolute top-10 left-1/2 transform -translate-x-1/2 lg:flex lg:justify-center lg:items-center w-14 h-14 rounded-xl hover:bg-white hover:bg-opacity-50"
@@ -34,7 +34,7 @@
 				</button>
 			</div>
 
-			<div class="overflow-hidden">
+			<div class="overflow-hidden lg:overflow-visible pb-24 lg:pb-0">
 				<div
 					class="transform lg:transform-none transition-all duration-300"
 					:class="showSidebar ? '-translate-x-full' : ''"
@@ -45,30 +45,114 @@
 						>
 							<div class="flex space-x-4 lg:space-x-10">
 								<div
-									class="bg-gray-300 rounded-xl h-28 w-full"
-								></div>
+									class="w-1/3 bg-white shadow-lg border border-gray-100 rounded-xl flex flex-col lg:flex-row items-center px-3 py-5 lg:p-5 space-x-0 lg:space-x-5"
+								>
+									<div
+										class="bg-indigo-50 p-2 rounded-lg text-blue-800 w-full lg:w-auto"
+									>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+											class="w-8 mx-auto"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+											/>
+										</svg>
+									</div>
+									<div>
+										<span
+											class="block uppercase text-xs text-gray-400 tracking-wider font-semibold"
+											>Visitors</span
+										>
+										<span
+											class="text-xl lg:text-2xl font-semibold"
+											>10,320</span
+										>
+									</div>
+								</div>
 								<div
-									class="bg-gray-300 rounded-xl h-28 w-full"
-								></div>
+									class="w-1/3 bg-white shadow-lg border border-gray-100 rounded-xl flex flex-col lg:flex-row items-center px-3 py-5 lg:p-5 space-x-0 lg:space-x-5"
+								>
+									<div
+										class="bg-indigo-50 p-2 rounded-lg text-blue-800 w-full lg:w-auto"
+									>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+											class="w-8 mx-auto"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
+											/>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
+											/>
+										</svg>
+									</div>
+									<div>
+										<span
+											class="block uppercase text-xs text-gray-400 tracking-wider font-semibold"
+											>Customers</span
+										>
+										<span
+											class="text-xl lg:text-2xl font-semibold"
+											>4,628</span
+										>
+									</div>
+								</div>
 								<div
-									class="bg-gray-300 rounded-xl h-28 w-full"
-								></div>
+									class="w-1/3 bg-red-500 shadow-lg border border-red-500 rounded-xl flex flex-col lg:flex-row items-center px-3 py-5 lg:p-5 space-x-0 lg:space-x-5"
+								>
+									<div
+										class="bg-red-700 p-2 rounded-lg text-white w-full lg:w-auto"
+									>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+											class="w-8 mx-auto"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
+											/>
+										</svg>
+									</div>
+									<div>
+										<span
+											class="block uppercase text-xs text-red-200 tracking-wider font-semibold"
+											>Orders</span
+										>
+										<span
+											class="text-xl lg:text-2xl font-semibold text-white"
+											>2,980</span
+										>
+									</div>
+								</div>
 							</div>
 
 							<div
 								class="bg-gray-300 rounded-xl h-28 mt-4 lg:mt-10 w-full"
 							></div>
 
-							<div
-								class="flex space-x-4 lg:space-x-10 mt-4 lg:mt-10"
-							>
-								<div
-									class="bg-gray-300 rounded-xl h-28 w-full"
-								></div>
-								<div
-									class="bg-gray-300 rounded-xl h-28 w-full"
-								></div>
-							</div>
+							<Blog />
 						</div>
 						<div
 							class="w-full flex-shrink-0 lg:w-1/4 lg:flex-shrink bg-gray-300 rounded-xl h-52"
@@ -85,10 +169,11 @@
 <script>
 import MainNav from "./components/MainNav.vue";
 import Tools from "./components/Tools.vue";
+import Blog from "./components/Blog.vue";
 
 export default {
 	name: "App",
-	components: { MainNav, Tools },
+	components: { MainNav, Tools, Blog },
 	data: function () {
 		return {
 			showSidebar: false,
---
+++ src/components/Blog.vue
@@ -0,0 +1,211 @@
+<template>
+	<div
+		class="md:flex space-y-5 md:space-y-0 md:space-x-5 lg:space-x-10 mt-4 lg:mt-10"
+	>
+		<div
+			class="w-full lg:w-1/2 bg-white shadow-lg border border-gray-100 rounded-xl p-5 space-y-5"
+		>
+			<div class="flex items-center">
+				<div class="w-10 h-10 rounded-full overflow-hidden mr-3">
+					<img
+						src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
+						alt=""
+						class="w-10 h-10 object-cover"
+					/>
+				</div>
+				<div>
+					<span class="block leading-3">Lilly Donovan</span>
+					<span class="text-gray-400 text-xs">Business trainer</span>
+				</div>
+			</div>
+
+			<h3 class="text-lg font-semibold">
+				<a href="#" class="hover:text-blue-800"
+					>How to properly manage your personal budget?</a
+				>
+			</h3>
+
+			<div class="flex items-center space-x-8">
+				<div class="flex items-center space-x-2">
+					<div class="w-6 text-blue-800">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+					<span class="text-sm text-gray-600">Video</span>
+				</div>
+
+				<div class="flex items-center space-x-2">
+					<div class="w-6 text-blue-300">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+					<span class="text-sm text-gray-600">15 min</span>
+				</div>
+
+				<div class="flex items-center space-x-2">
+					<div class="w-6 text-red-500">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+							/>
+						</svg>
+					</div>
+					<span class="text-sm text-gray-600">112 likes</span>
+				</div>
+			</div>
+
+			<div class="flex justify-between items-center">
+				<div class="text-gray-600 font-semibold">5 days ago</div>
+
+				<a
+					href="#"
+					class="bg-blue-800 text-white rounded-full hover:bg-red-500 px-4 py-3 flex items-center space-x-3"
+				>
+					<span>Connect</span>
+
+					<div class="w-4 h-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<div
+			class="w-full lg:w-1/2 bg-white shadow-lg border border-gray-100 rounded-xl p-5 space-y-5"
+		>
+			<div class="flex items-center">
+				<div class="w-10 h-10 rounded-full overflow-hidden mr-3">
+					<img
+						src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
+						alt=""
+						class="w-10 h-10 object-cover"
+					/>
+				</div>
+				<div>
+					<span class="block leading-3">Lilly Donovan</span>
+					<span class="text-gray-400 text-xs">Business trainer</span>
+				</div>
+			</div>
+
+			<h3 class="text-lg font-semibold">
+				<a href="#" class="hover:text-blue-800"
+					>How to properly manage your personal budget?</a
+				>
+			</h3>
+
+			<div class="flex items-center space-x-8">
+				<div class="flex items-center space-x-2">
+					<div class="w-6 text-blue-800">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+					<span class="text-sm text-gray-600">Video</span>
+				</div>
+
+				<div class="flex items-center space-x-2">
+					<div class="w-6 text-blue-300">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+					<span class="text-sm text-gray-600">15 min</span>
+				</div>
+
+				<div class="flex items-center space-x-2">
+					<div class="w-6 text-red-500">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+							/>
+						</svg>
+					</div>
+					<span class="text-sm text-gray-600">112 likes</span>
+				</div>
+			</div>
+
+			<div class="flex justify-between items-center">
+				<div class="text-gray-600 font-semibold">5 days ago</div>
+
+				<a
+					href="#"
+					class="bg-blue-800 text-white rounded-full hover:bg-red-500 px-4 py-3 flex items-center space-x-3"
+				>
+					<span>Connect</span>
+
+					<div class="w-4 h-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Blog",
+};
+</script>
