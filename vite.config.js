d4dd6c44ecfc3d7f990ce739976cc4dcfab25eb1
lukeska <luca.ubiali@gmail.com>
2023-10-06 15:41:15 +0200

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "responsive-dashboard",
+	base: "tailwind-responsive-dashboard",
 	plugins: [vue()],
 });
