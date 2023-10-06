42032eebe80157b021d16c763000dbe84f98870f
lukeska <luca.ubiali@gmail.com>
2023-10-06 15:39:22 +0200

    fix github pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
-module.exports = {
-	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	darkMode: false, // or 'media' or 'class'
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {
-			padding: {
-				"3/5": "60%",
-			},
-		},
-	},
-	variants: {
-		extend: {
-			translate: ["responsive", "group-hover", "hover", "focus"],
-		},
+		extend: {},
 	},
 	plugins: [],
 };
